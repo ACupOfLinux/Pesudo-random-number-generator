4d69f18ca8bc8b00fb3a57ebd9e9fcc66c1f64b9
kayden <kayden@DESKTOP-KKBT932>
2022-09-27 17:48:25 -0700

    Add project files.

---
+++ NumberGenerator/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace NumberGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random number = new Random();
+
+            int num = number.Next(1, 10000);
+            Console.WriteLine("Your pesudo random number is " + num);
+
+
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+            Console.Beep();
+        }
+    }
+}
+
